567fb2ba835af230692b8bc66af190903d5ae7a4
burgueee <burguee16@gmail.com>
2024-09-18 08:15:32 -0300

    Miercoles 18 de septiembre FINISH?

--- Notess/Views/NotePage.xaml.cs
+++ Notess/Views/NotePage.xaml.cs
@@ -1,56 +1,10 @@
 namespace Notes.Views;
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 
 public partial class NotePage : ContentPage
 {
-    public string ItemId
-    {
-        set { LoadNote(value); }
-    }
-
     public NotePage()
     {
         InitializeComponent();
-
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
-    }
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-            File.WriteAllText(note.FileName, TextEditor.Text);
-    }
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-        {
-            // Delete the file.
-            if (File.Exists(note.FileName))
-                File.Delete(note.FileName);
-        }
-
-        await Shell.Current.GoToAsync("..");
-    }
-    private void LoadNote(string fileName)
-    {
-        Models.Note noteModel = new Models.Note();
-        noteModel.FileName = fileName;
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
     }
 }
-
-
-
-
-
-
 
